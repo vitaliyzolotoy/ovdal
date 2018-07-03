422aadbd2dd0de7ac79eff0fa3cf77e2800ab05e
Vitaly Zolotoy <vitaliyzolotoy@gmail.com>
2018-07-03 12:15:23 +0300

    Types of response

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/internal/operators';
+import {User} from '../models/user';
+import {Repo} from '../models/repo';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,7 @@ export class GithubService {
   constructor(private _http: HttpClient) { }
 
   getUser(userName) {
-    return this._http.get('http://api.github.com/users/' + userName
+    return this._http.get<User>('http://api.github.com/users/' + userName
       + '?client_id=' + this.clientId
       + '&client_secret=' + this.clientSecret)
       .pipe(
@@ -21,7 +23,7 @@ export class GithubService {
   }
 
   getRepos(userName) {
-    return this._http.get('http://api.github.com/users/' + userName
+    return this._http.get<Repo[]>('http://api.github.com/users/' + userName
       + '/repos?client_id=' + this.clientId
       + '&client_secret=' + this.clientSecret)
       .pipe(
